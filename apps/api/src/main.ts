8ead64ea0bb7521f040fce98433e87923679bf04
Amazingmercy <amaefulemercy499@gmail.com>
2026-09-15 22:28:23 +0100

    modify for production

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   app.enableCors({
     origin: process.env.ALLOWED_ORIGINS
       ? process.env.ALLOWED_ORIGINS.split(',')
-      : ['http://localhost:3000', 'http://localhost:5173'],
+      : ['http://localhost:3000', 'http://localhost:5173', 'https://fulafia-repository-web.vercel.app', 'https://fulafia-repository-api.onrender.com'],
     credentials: true,
   });
 
